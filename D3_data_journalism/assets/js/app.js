bdda0763a991c33fc10234d503de1df5216654f8
mkrenicki <73031837+mkrenicki@users.noreply.github.com>
2021-03-03 06:52:14 -0500

    starting on app

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,14 @@
 // @TODO: YOUR CODE HERE!
+// Modeled most of the structuring of my code based on excercise 16.3.9 we did in class
+var svgWidth = 900;
+var svgHeight = 500;
+​
+var margin = {
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: 60
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
