6ff08b10dd39867a195ca3da80935d2a7b2d8943
mkrenicki <73031837+mkrenicki@users.noreply.github.com>
2021-03-03 08:28:24 -0500

    scatterplot loaded. now detailing

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -1,26 +1,59 @@
 // @TODO: YOUR CODE HERE!
 // Modeled most of the structuring of my code based on excercise 16.3.9 we did in class
-var svgWidth = 900;
+var svgWidth = 960;
 var svgHeight = 500;
-​
+
 var margin = {
   top: 20,
-  right: 20,
-  bottom: 30,
-  left: 60
+  right: 40,
+  bottom: 60,
+  left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // SVG wrapper and append an SVG group that will hold our chart.
-var svg = d3.select("body")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-​
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var svg = d3.select("body").append("svg").attr("width", svgWidth).attr("height", svgHeight);
+
+var chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("data.csv").then(function(data)
+d3.csv("data.csv").then(function(csvData) {
+    // Step 1: Parse Data/Cast as numbers- age vs. smokers
+    // ==============================
+    csvData.forEach(function(data) {
+      data.age = +data.age;
+      data.smokes = +data.smokes;
+    });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear().domain([20, d3.max(csvData, d => d.age)]).range([0, width]);
+    var yLinearScale = d3.scaleLinear().domain([0, d3.max(csvData, d => d.smokes)]).range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(csvData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.age))
+    .attr("cy", d => yLinearScale(d.smokes))
+    .attr("r", "10")
+    .attr("fill", "green")
+    .attr("opacity", ".4");
+});
